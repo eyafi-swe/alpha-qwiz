5d24decf2867ace0873da9db03f0b4c25cafb485
Sarwar Azmain Eyafi <75948244+EyafiNeo@users.noreply.github.com>
2022-10-10 23:09:39 +0600

    Header check

--- src/Components/Chapters/Chapters.js
+++ src/Components/Chapters/Chapters.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Chapters = () => {
     return (
-        <div>
-            This is chapters home
+        <div className='mt-5 container mx-auto'>
+            <div className='flex justify-center bg-sky-300 p-4 rounded-lg md:flex-row flex-col'>
+                <img src="https://i.ibb.co/bR7d9mQ/pngwing-com.png" alt="Students" border="0" />
+                <div className='flex flex-col items-center'>
+                    <img src="https://i.ibb.co/sp7LBn9/pngwing-com.png" alt="pngwing-com" border="0" className='w-1/3' />
+                    <p className='text-4xl text-center font-semibold text-orange-600'>Participate this awesome quiz and test your knowledge</p>
+                </div>
+                
+            </div>
         </div>
     );
 };
--- src/Components/NavLinks/NavLinks.js
+++ src/Components/NavLinks/NavLinks.js
@@ -3,10 +3,16 @@ import { NavLink } from 'react-router-dom';
 
 const NavLinks =(props) => {
     const {rout} = props;
+    let activeStyle = {
+        borderBottom: "1px solid red",
+      };
+    
     return (
         <div className=''>
             {/* <a href= {rout.path} className='mx-4 hover:text-rose-700'>{rout.name}</a> */}
-            <NavLink className='mx-4 hover:bg-slate-400 px-2 py-1'  to = {rout.path}>{rout.name}</NavLink>
+            <NavLink className='mx-4 hover:bg-slate-400 px-2 py-1' to = {rout.path} style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>{rout.name}</NavLink>
         </div>
     );
 };
