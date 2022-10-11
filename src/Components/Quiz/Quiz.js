e43cd429fcc2f24102cc01dac99b3f45689fa683
Sarwar Azmain Eyafi <75948244+EyafiNeo@users.noreply.github.com>
2022-10-11 15:45:49 +0600

    Qsn answer check

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -1,37 +1,21 @@
-import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+
 import QuizQuestion from '../QuizQuestion/QuizQuestion';
 
 const Quiz = () => {
     const quizData = useLoaderData()
     const { data } = quizData;
     const { name, questions, total } = data;
-    const [selected,setSelected] = useState('');
-
-    const handleClick = (question)=>{
-        const {correctAnswer} = question;
-        console.log(correctAnswer,selected)
-        if(correctAnswer === selected){
-            console.log('Nais')
-        }
-        else{
-            console.log('Not cool')
-        }
-    }
-    // const onChangeValue = (e) => {
-    //     const selected_answer = e.target.value;
-    //     setSelected(selected_answer);
-    // }
-   
+       
+    
     return (
         <div className='mt-10 text-center text-orange-800'>
             <h1 className=' text-2xl font-semibold'>Quiz Topic: {name}</h1>
             <p className='font-semibold text-lg'>Total Questions: {total}</p>
             {
-                questions.map(qstion => {
-                    return <QuizQuestion key={qstion.id} qstion={qstion} selected = {selected} setSelected = {setSelected} handleClick = {handleClick}></QuizQuestion>
-                })
+                questions.map((qstion, serial = 1) => <QuizQuestion key={qstion.id} qstion={qstion} serial = {serial}></QuizQuestion>)
             }
+            
         </div>
     );
 };
--- src/Components/QuizQuestion/QuizQuestion.js
+++ src/Components/QuizQuestion/QuizQuestion.js
@@ -1,8 +1,23 @@
 import { EyeIcon } from '@heroicons/react/24/solid';
 import React from 'react';
-
-const QuizQuestion = ({ qstion, selected, setSelected, handleClick }) => {
-    const { options, question } = qstion;
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+const QuizQuestion = ({ qstion, serial }) => {
+    const { options, question, correctAnswer } = qstion;
+    let isCorrect;
+    const handleClick = (selected) => {
+        if (correctAnswer === selected) {
+            console.log('Thik ache')
+            isCorrect = true;
+        }
+        else {
+            console.log('Na vai')
+            isCorrect = false
+        }
+        console.log(correctAnswer, selected, isCorrect)
+    }
+    const notify = () => isCorrect ? toast("Wow so easy!") : toast("Not Cool! You may google it.");
+    const showCorrectAnswer = () => toast(correctAnswer);
     return (
         <div className='border container lg:w-1/2 mx-auto bg-slate-100 mt-10 px-5 py-2 rounded-lg'>
             <div className='grid grid-cols-6 mb-2'>
@@ -10,26 +25,28 @@ const QuizQuestion = ({ qstion, selected, setSelected, handleClick }) => {
 
                 </div>
                 <div className=' col-span-4 '>
-                    <p className='text-orange-800 font-semibold w-4/5 mx-auto '>Question: {question}</p>
+                    <p className='text-orange-800 font-semibold w-4/5 mx-auto '>Question {serial+1}:  {question}</p>
                 </div>
                 <div className=' flex justify-end'>
-                    <EyeIcon className='h-5 w-5'></EyeIcon>
+                    <EyeIcon className='h-5 w-5 cursor-pointer' onClick={showCorrectAnswer}></EyeIcon>
+                    <ToastContainer />
                 </div>
             </div>
             <div className='grid md:grid-cols-2 gap-4 text-white' >
                 {
-                    options.map(option => {
-                        return <div className=' bg-orange-800 py-5 px-1 rounded-lg text-center' onClick={() => {
-                            setSelected(option);
-                          }}>
-                            <label className='cursor-pointer' >
-                                <input type="radio" name='selected-answer' value={option} onClick={() => handleClick(qstion)}  />
-                                <span className='ml-1'>{option}</span>
-                            </label>
-                        </div>
-                    })
+                    options.map((option, i = 0) => <div className=' bg-orange-800 py-5 px-1 rounded-lg text-center' key={i}>
+                        <label className='cursor-pointer hover:text-gray-300' >
+                            <span className='hidden'>{i++}</span>
+                            <input type="radio" name='selected-answer' value={option} onClick={() => { handleClick(option); notify() }} />
+                            <span className='ml-1'>{option}</span>
+                            <ToastContainer />
+                        </label>
+                    </div>)
                 }
+
+
             </div>
+
         </div>
     );
 };
