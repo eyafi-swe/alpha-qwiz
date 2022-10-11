dd329ac4295c3da5d449b88067b22e96db28a298
Sarwar Azmain Eyafi <75948244+EyafiNeo@users.noreply.github.com>
2022-10-11 12:04:00 +0600

    Quiz qsns check

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import QuizQuestion from '../QuizQuestion/QuizQuestion';
 
@@ -6,13 +6,30 @@ const Quiz = () => {
     const quizData = useLoaderData()
     const { data } = quizData;
     const { name, questions, total } = data;
+    const [selected,setSelected] = useState('');
+
+    const handleClick = (question)=>{
+        const {correctAnswer} = question;
+        console.log(correctAnswer,selected)
+        if(correctAnswer === selected){
+            console.log('Nais')
+        }
+        else{
+            console.log('Not cool')
+        }
+    }
+    // const onChangeValue = (e) => {
+    //     const selected_answer = e.target.value;
+    //     setSelected(selected_answer);
+    // }
+   
     return (
         <div className='mt-10 text-center text-orange-800'>
             <h1 className=' text-2xl font-semibold'>Quiz Topic: {name}</h1>
             <p className='font-semibold text-lg'>Total Questions: {total}</p>
             {
                 questions.map(qstion => {
-                    return <QuizQuestion key={qstion.id} qstion={qstion}></QuizQuestion>
+                    return <QuizQuestion key={qstion.id} qstion={qstion} selected = {selected} setSelected = {setSelected} handleClick = {handleClick}></QuizQuestion>
                 })
             }
         </div>
--- src/Components/QuizQuestion/QuizQuestion.js
+++ src/Components/QuizQuestion/QuizQuestion.js
@@ -1,9 +1,8 @@
 import { EyeIcon } from '@heroicons/react/24/solid';
 import React from 'react';
 
-const QuizQuestion = ({ qstion }) => {
-    console.log(qstion);
-    const { options, question, correctAnswer } = qstion;
+const QuizQuestion = ({ qstion, selected, setSelected, handleClick }) => {
+    const { options, question } = qstion;
     return (
         <div className='border container lg:w-1/2 mx-auto bg-slate-100 mt-10 px-5 py-2 rounded-lg'>
             <div className='grid grid-cols-6 mb-2'>
@@ -17,9 +16,18 @@ const QuizQuestion = ({ qstion }) => {
                     <EyeIcon className='h-5 w-5'></EyeIcon>
                 </div>
             </div>
-            <div className='grid md:grid-cols-2 gap-4 text-white'>
+            <div className='grid md:grid-cols-2 gap-4 text-white' >
                 {
-                    options.map(option => <p className='border bg-orange-800 py-5 rounded-lg text-center'>{option}</p>)
+                    options.map(option => {
+                        return <div className=' bg-orange-800 py-5 px-1 rounded-lg text-center' onClick={() => {
+                            setSelected(option);
+                          }}>
+                            <label className='cursor-pointer' >
+                                <input type="radio" name='selected-answer' value={option} onClick={() => handleClick(qstion)}  />
+                                <span className='ml-1'>{option}</span>
+                            </label>
+                        </div>
+                    })
                 }
             </div>
         </div>
