c082b10bc455521116bfb5121c351014b92beb7b
Sarwar Azmain Eyafi <75948244+EyafiNeo@users.noreply.github.com>
2022-10-12 22:57:11 +0600

    Active navlink styling check

--- src/Components/NavLinks/NavLinks.js
+++ src/Components/NavLinks/NavLinks.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
-const NavLinks =(props) => {
-    const {rout} = props;
-    let activeStyle = {
-        borderBottom: "1px solid white",
-      };
-
-    return (
-        <div className=''>
-            <NavLink className='mx-4 hover:bg-slate-400 px-2 py-1' to = {rout.path} style={({ isActive }) =>
-              isActive ? activeStyle : undefined
-            }>{rout.name}</NavLink>
-        </div>
-    );
+const NavLinks = (props) => {
+  const { rout } = props;
+  const location = useLocation();
+  return (
+    <div className=''>
+      <NavLink className = {`mx-4 hover:bg-sky-400  rounded px-2 py-1 ${location.pathname === rout.path ? 'bg-sky-400' : ''}`} to={rout.path}>{rout.name}</NavLink>
+      {/* <NavLink className = 'mx-4 hover:bg-slate-400 px-2 py-1' to={rout.path} style={({ isActive }) =>
+        isActive ? activeStyle : undefined
+      }>{rout.name}</NavLink> */}
+    </div>
+  );
 };
 
 export default NavLinks;
