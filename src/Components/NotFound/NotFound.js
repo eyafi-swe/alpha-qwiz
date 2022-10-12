c7d604322ffb4edac431ca5a94d81942caebf292
Sarwar Azmain Eyafi <75948244+EyafiNeo@users.noreply.github.com>
2022-10-12 23:08:22 +0600

    Final touch

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div className=' flex flex-col lg:flex-row items-center justify-center border ' style={{ height: "90vh" }}>
+        <div className=' flex flex-col lg:flex-row items-center justify-center ' style={{ height: "90vh" }}>
             <img src="https://i.ibb.co/M7rhNCB/pngwing-com.png" alt="pngwing-com" border="0" className='h-80' />
             <p className='text-4xl text-center text-red-600 font-extrabold lg:ml-10 mt-10'>Oh NO!! PAGE NOT FOUND!!</p>
         </div>
